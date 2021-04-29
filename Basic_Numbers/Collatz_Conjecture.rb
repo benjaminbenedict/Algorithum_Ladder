cc42d65c8916d65025bec9c72c2669db2ab299ec
Ben Benedict <Ben.Benedict@gmail.com>
2021-04-29 14:09:36 -0500

    Collatz Contjecture itteration and reccursion

---
+++ Basic_Numbers/Collatz_Conjecture.rb
@@ -0,0 +1,50 @@
+# The Collatz Conjecture or 3x+1 problem can be summarized as follows:
+
+# Take any positive integer n. If n is even, divide n by 2 to get n / 2. If n is odd, multiply n by 3 and add 1 to get 3n + 1. Repeat the process indefinitely. The conjecture states that no matter which number you start with, you will always reach 1 eventually.
+
+# Given a number n, return the number of steps required to reach 1.
+
+# Examples
+# Starting with n = 12, the steps would be as follows:
+
+# 12
+# 6
+# 3
+# 10
+# 5
+# 16
+# 8
+# 4
+# 2
+# 1
+
+# Resulting in 9 steps. So for input n = 12, the return value would be 9.
+
+def coll_conject(int)
+  num = int
+  steps = 0
+  while num > 1
+    if num.even?
+      num = num / 2
+    else
+      num = (num * 3) + 1
+    end
+    steps += 1
+  end
+  return steps
+end
+
+def coll_conject_fancy_reccursion(number)
+  return_value = [number]
+  return return_value if number == 1
+
+  if number.even?
+    return_value + coll_conject_fancy_reccursion(number / 2)
+  else
+    return_value + coll_conject_fancy_reccursion((number * 3) + 1)
+  end
+end
+
+p coll_conject(12)
+
+p coll_conject_fancy_reccursion(12)
